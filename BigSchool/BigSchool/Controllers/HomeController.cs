561450729c5a9a35bbc6cdeef9877adb671391a8
VietCuong2503 <pvc.cuong.pvc@gmail.com>
2021-07-21 23:29:56 +0700

    Initial commit

---
+++ BigSchool/BigSchool/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using BigSchool.ViewModels;
+using Microsoft.AspNet.Identity;
+
+namespace BigSchool.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var upcommingCourses = _dbContext.Courses
+               .Include(c => c.Lecturer)
+               .Include(c => c.Category)
+               .Where(c => c.DateTime > DateTime.Now);
+
+            var viewModel = new CourseViewModel
+            {
+                UpcommingCourses = upcommingCourses,
+                ShowAction = User.Identity.IsAuthenticated
+            };
+            string userID = User.Identity.GetUserId();
+            ViewBag.list = _dbContext.Followings.Where(x => x.FollowerId == userID).Select(x => x.FolloweeId).ToList();
+            ViewBag.listfl = _dbContext.Attendances.Where(a => a.AttendeeId == userID).Select(a => a.CourseId).ToList();
+            return View(viewModel);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BigSchool/BigSchool/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using BigSchool.Models;
+namespace BigSchool.Models
+{
+    public class Category
+    {
+        public byte Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
